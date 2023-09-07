fb3256ee67f104919324f902e756a6e74c39cb25
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-07 17:36:21 +0100

    fix sign in

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { Suspense } from "react";
 
 import "./App.css";
 import Home from "./Pages/Home";
-import { NavBar } from "./Component/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Component/layout";
 import Footer from "./Component/footer";
 import Privacy from "./Pages/Privacy";
 import Terms from "./Pages/Terms";
 import ChatBot from "./Component/ChatBot";
+import { NavBar } from "./Component/navBar";
 
 function App() {
   return (
